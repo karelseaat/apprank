dc68da9d19d9130e24f2e6221e2246beb3d589f1
Satan <karelseaat@gmail.com>
2022-01-22 18:28:32 +0100

    adding more log

--- cronscripts/seleniumsearchfind.py
+++ cronscripts/seleniumsearchfind.py
@@ -58,8 +58,8 @@ results = session.query(Searchkey).all()
 
 
 for result in results:
-    ser = Service("../geckodriver")
-    driver = webdriver.Firefox(service=ser, options=options, service_log_path = "./logit.log")
+    ser = Service("../geckodriver", log_path="service_log_path.log")
+    driver = webdriver.Firefox(service=ser, options=options,)
     driver.get(f'https://play.google.com/store/search?q={result.searchsentence}&c=apps')
     last_height = driver.execute_script("return document.body.scrollHeight")
 
