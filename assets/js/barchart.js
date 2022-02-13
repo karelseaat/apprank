c7907fe02709d6578c34eb0776a8b5d85e2d0af4
Satan <karelseaat@gmail.com>
2022-02-14 00:12:04 +0100

    prep for bars

--- assets/js/barchart.js
+++ assets/js/barchart.js
@@ -1,84 +1,27 @@
-"use strict";
+// Bar chart
 
-var canvas = document.getElementById('big-line-chart')
-
-var data_array = JSON.parse(document.getElementById('apps').innerHTML)
-
-var allvalues = [];
-for(var key in data_array) {
-
-    for(var thekey in data_array[key]['data']) {
-      if (data_array[key]['data'][thekey]['y'] !== "null")
-      {
-        allvalues.push(data_array[key]['data'][thekey]['y'])
+var settings = {
+    type: 'bar',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
       }
-
     }
 }
 
 
-var ctx = canvas.getContext('2d');
-var newchart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    datasets: data_array,
-    labels: JSON.parse(document.getElementById('labels').innerHTML)
-  },
-  options: {
+var blaat = document.getElementById("klont").getAttribute('names');
 
-    maintainAspectRatio: false,
-    legend: {
-      display: true
-    },
-    responsive: true,
-    tooltips: {
-      backgroundColor: '#f5f5f5',
-      titleFontColor: '#333',
-      bodyFontColor: '#666',
-      bodySpacing: 4,
-      xPadding: 12,
-      mode: 'nearest',
-      intersect: 0,
-      position: 'nearest'
-    },
-    scales: {
-      yAxes: [{
-
-        scaleLabel: {
-        display: true,
-        labelString: 'store pos'
-      },
-        barPercentage: 1.6,
-        gridLines: {
-          drawBorder: false,
-          color: 'rgba(29,140,248,0.0)',
-          zeroLineColor: 'transparent'
-        },
-        ticks: {
-          beginAtZero: true,
-          reverse: true,
-          padding: 20,
-          fontColor: '#9a9a9a',
-          min: Math.min.apply(this, allvalues) - 2,
-          max: Math.max.apply(this, allvalues) + 2
-        }
-      }],
-      xAxes: [{
-        scaleLabel: {
-        display: true,
-        labelString: 'At date'
-      },
-        barPercentage: 1.6,
-        gridLines: {
-          drawBorder: false,
-          color: 'rgba(225,78,202,0.1)',
-          zeroLineColor: 'transparent'
-        },
-        ticks: {
-          padding: 20,
-          fontColor: '#9a9a9a'
-        }
-      }]
-    }
-  }
-});
+JSON.parse(blaat).forEach(element => new Chart(document.getElementById(element + "-canvas"), settings));
